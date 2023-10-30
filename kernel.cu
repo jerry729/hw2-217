9e2e7e8916f0dda73696564f345c05c3f9950548
Zhaorui Yang <zyang247@ucr.edu>
2023-10-30 04:42:40 +0000

    first commit

--- kernel.cu
+++ kernel.cu
@@ -19,6 +19,26 @@ __global__ void naiveReduction(float *out, float *in, unsigned size)
 
     // INSERT KERNEL CODE HERE
     // NAIVE REDUCTION IMPLEMENTATION
+    __shared__ float partialSum[2*BLOCK_SIZE];
+
+    unsigned int t = threadIdx.x;
+    unsigned int start = 2 * blockIdx.x * blockDim.x;
+
+    partialSum[t] = in[start + t];
+    partialSum[blockDim.x + t] = in[start + blockDim.x + t];
+
+    for (unsigned int stride = 1; stride <= blockDim.x; stride *= 2)
+    {
+        __syncthreds();
+        if (t % stride == 0)
+        {
+            partialSum[2*t] += partialSum[2*t+stride];
+        }
+    }
+
+    
+    unsigned int outidx = blockIdx.x;
+    out[outidx] = partialSum[0];
 
 }
 
@@ -32,6 +52,24 @@ __global__ void optimizedReduction(float *out, float *in, unsigned size)
 
     // INSERT KERNEL CODE HERE
     // OPTIMIZED REDUCTION IMPLEMENTATION
+    __shared__ float partialSum[2*BLOCK_SIZE];
 
+    unsigned int t = threadIdx.x;
+    unsigned int start = 2 * blockIdx.x * blockDim.x;
+
+    partialSum[t] = in[start + t];
+    partialSum[blockDim.x + t] = in[start + blockDim.x + t];
+
+    for (unsigned int stride = blockDim.x; stride >= 1; stride /= 2)
+    {
+        __syncthreds();
+        if (t <= blockDim.x / 2)
+        {
+            partialSum[t] += partialSum[t+stride];
+        }
+    }
+
+    unsigned int outidx = blockIdx.x;
+    out[outidx] = partialSum[0];
 
 }
